12d441a8364a60edd6e5870d7dfb6efa92a07aba
Miguel Duran :) <miguel.duran.14.17@outlook.com>
2026-04-22 18:33:55 -0500

    rrr

--- mongo/test/test.js
+++ mongo/test/test.js
@@ -1,1 +1,32 @@
-jbh
+import { expect } from 'chai';
+import { login } from '../controllers/usuario.js';
+
+describe('Pruebas de Usuario con Chai', () => {
+  it('Debería fallar si el correo no está registrado', async () => {
+  // 1. El "req" con un correo que no existe
+  const req = {
+    body: {
+      email: 'no-existoo@correo.com',
+      password: '123456password'
+    }
+  };
+
+  // 2. El "res" simulado para capturar lo que responda el controlador
+  const res = {
+    statusCode: 0,
+    message: '',
+    status: function(code) {
+      this.statusCode = code;
+      return this; // Para poder encadenar .json()
+    },
+    json: function(data) {
+      this.message = data.msg;
+    }
+  };
+
+  // 3. Ejecutamos la función
+  await login(req, res);
+
+  // 4. ¡Aquí es donde usas a Chai!
+});
+});
